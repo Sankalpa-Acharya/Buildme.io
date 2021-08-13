a9203aad1bef3fee9177c2fc6d1d4284cb3d824e
Sankalpa Acharya <79009768+Sankalpa-Acharya@users.noreply.github.com>
2021-08-13 19:19:07 +0545

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,20 +2,27 @@ number_of_row=0;
 number_of_column=0;
 let addrow=document.getElementById('row');
 let addcolumn=document.getElementById('column');
+let adddiv=document.getElementById('div');
 addrow.addEventListener('click',addingrow)
 function addingrow(){
-    let main_section=document.querySelector('.main-section')
-    main_section.innerHTML=main_section.innerHTML+'<div></div>'
     number_of_row++
-    main_section.setAttribute('style',`grid-template-rows:repate(${number_of_row},1fr)`)
+    let main_section=document.querySelector('.main-section')
+    main_section.setAttribute('style',`grid-template-rows:repeat(${number_of_row},1fr);grid-template-columns: repeat(${number_of_column},1fr)`)
 }
 
 
 addcolumn.addEventListener('click',addingcolumn)
-
 function addingcolumn(){
+    number_of_column++
+    let main_section=document.querySelector('.main-section')
+    main_section.setAttribute('style',`grid-template-columns: repeat(${number_of_column},1fr);grid-template-rows:repeat(${number_of_row},1fr)`)
+}
+
+
+adddiv.addEventListener('click',addingdiv)
+function addingdiv(){
     let main_section=document.querySelector('.main-section')
     main_section.innerHTML=main_section.innerHTML+'<div></div>'
-    number_of_column++
-    main_section.setAttribute('style',`grid-template-columns:repate(${number_of_column},1fr)`)
+
+    
 }
